19583559a12db8d16bbdf7b506f0dede72482bba
alanft <alanft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-19 10:42:01 +0000

    New 2.5 branch for alpha testing
    git-svn-id: https://plugins.svn.wordpress.org/widget-logic/branches/alpha2_5@41538 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget_logic.php
+++ widget_logic.php
@@ -1,83 +1,82 @@
 <?php
 /*
 Plugin Name: Widget Logic
-Plugin URI: http://freakytrigger.co.uk/
+Plugin URI: http://freakytrigger.co.uk/wordpress-setup/
 Description: Allows widgets to appear as directed by WP tags is_home etc
 Author: Alan Trewartha
-Version: 0.3
+Version: 0.39
 Author URI: http://freakytrigger.co.uk/author/alan/
 */ 
 
 
 
-// before the page is drawn add minimal controls to ALL widgets and make control boxen deeper
-// needs to happen before wp_widgets_admin_head
-add_action( 'admin_head', 'widget_logic_expand_control', 5); 
+// re-wire the registered control functions to go via widget_logic_extra_control
+add_action( 'sidebar_admin_setup', 'widget_logic_expand_control'); 
 function widget_logic_expand_control()
-{	global $wp_registered_widgets, $wp_registered_widget_controls;
-	foreach ( $wp_registered_widgets as $name => $widget )
-	{	if ( !isset( $wp_registered_widget_controls[$name] ) )
-			register_widget_control($widget['name'], 'widget_logic_empty_control', 250,61);
-		$wp_registered_widget_controls[$name]['height']+=40;
-	}
-
-}
-
-function widget_logic_empty_control() {}
-
-// after the controls drawn, add INPUT by plaing with DOM
-add_action('sidebar_admin_page', 'widget_logic_add_control');
-function widget_logic_add_control()
-{	global $wp_registered_widget_controls;
+{	global $wp_registered_widgets, $wp_registered_widget_controls, $wp_version;
 
 	if(!$wl_options = get_option('widget_logic')) $wl_options = array();
 
-	?><script>
-		function insert_control(id,value)
-		{	nc = document.createElement("p");
-			nc.innerHTML="<label for='" + id + "-widget_logic'>Widget logic <input type='text' name='" + id + "-widget_logic' id='" + id + "-widget_logic' value='" + value + "' /></label>";
-			document.getElementById(id + "control").getElementsByTagName("div").item(0).appendChild(nc);
+	foreach ( $wp_registered_widgets as $id => $widget )
+	{	if (!$wp_registered_widget_controls[$id])
+				register_widget_control($widget['name'], 'widget_logic_empty_control', 250,61);
+				
+		if ( version_compare($wp_version, '2.5', ">="))
+		{	$wp_registered_widget_controls[$id]['params'][0]['id_for_wl']=$id;
 		}
-	<?	
-	foreach ( $wp_registered_widget_controls as $name => $widget )
-	{	$id=$widget['id'];
+		else
+		{	array_push($wp_registered_widget_controls[$id]['params'],$id);	
+			$wp_registered_widget_controls[$id]['height']+=40;
+		}
+			
+		$wp_registered_widget_controls[$id]['callback_wl_redirect']=$wp_registered_widget_controls[$id]['callback'];
+		$wp_registered_widget_controls[$id]['callback']='widget_logic_extra_control';		
 
 		if (isset($_POST[$id.'-widget_logic']))
-		{	$wl_options[$id]=$_POST[$id.'-widget_logic'];
-			update_option('widget_logic', $wl_options);
-		}
-		echo "\n\t\tinsert_control('".$id."', '".htmlspecialchars(stripslashes($wl_options[$id]),ENT_QUOTES)."');";
+			$wl_options[$id]=$_POST[$id.'-widget_logic'];
 	}
-	echo "\n\t</script>\n\t";
+	update_option('widget_logic', $wl_options);
 
+//	print_r($wp_registered_widget_controls);
+}
+function widget_logic_empty_control() {}
 
-	if ( isset($_POST['widget_logic-options-submit']) )
-	{	$wl_options['widget_logic-options-filter']=$_POST['widget_logic-options-filter'];
-		update_option('widget_logic', $wl_options);
-	}
-	?><div class="wrap">
-		<form method="POST">
-			<h2>Widget Logic options</h2>
-			<p style="line-height: 30px;">
-			<label for="widget_logic-options-filter">Use 'widget_content' filter?
-			<input id="widget_logic-options-filter" name="widget_logic-options-filter" type="checkbox" value="checked" class="checkbox" <? echo $wl_options['widget_logic-options-filter'] ?> /></label>
-			<span class="submit"><input type="submit" name="widget_logic-options-submit" id="widget_logic-options-submit" value="Save" /></span></p>
-		</form>
-	</div>
-	<?php
+function widget_logic_extra_control()
+{	global $wp_registered_widget_controls, $wp_version;
+	$params=func_get_args();
 
+	if ( version_compare($wp_version, '2.5', ">="))
+		$id=$params[0]['id_for_wl'];
+	else
+		$id=array_pop($params);	
+
+	$callback=$wp_registered_widget_controls[$id]['callback_wl_redirect'];
+
+	if (is_callable($callback))
+		call_user_func_array($callback, $params);		// go to the original control function
+
+	if(!$wl_options = get_option('widget_logic')) $wl_options = array();
+	
+
+	$id_disp=$id;
+	$value=htmlspecialchars(stripslashes($wl_options[$id]),ENT_QUOTES);
+	if (isset($params[0]['number'])) $number=$params[0]['number'];
+	if ($number==-1) {$number="%i%"; $value="";}
+	if (isset($number)) $id_disp=$wp_registered_widget_controls[$id]['id_base'].'-'.$number;
+
+	echo "<p><label for='".$id_disp."-widget_logic'>Widget logic <input type='text' name='".$id_disp."-widget_logic' id='".$id_disp."-widget_logic' value='".$value."' /></label></p>";
 
 }
 
 
-// intercept EVERY registered widget - redirect it and put its ID on the end of the params
+// intercept  registered widgets - redirect it and put its ID on the end of the params
 // perhaps there is a way to just intercept the ones that are used??
 add_action('wp_head', 'widget_logic_redirect_callback');
 function widget_logic_redirect_callback()
 {	global $wp_registered_widgets;
 	foreach ( $wp_registered_widgets as $id => $widget )
 	{	array_push($wp_registered_widgets[$id]['params'],$id);
-		$wp_registered_widgets[$id]['callback_redirect']=$wp_registered_widgets[$id]['callback'];
+		$wp_registered_widgets[$id]['callback_wl_redirect']=$wp_registered_widgets[$id]['callback'];
 		$wp_registered_widgets[$id]['callback']='widget_logic_redirected_callback';
 	}
 }
@@ -87,7 +86,7 @@ function widget_logic_redirected_callback()
 {	global $wp_registered_widgets;
 	$params=func_get_args();										// get all the passed params
 	$id=array_pop($params);											// take off the widget ID
-	$callback=$wp_registered_widgets[$id]['callback_redirect'];		// find the real callback
+	$callback=$wp_registered_widgets[$id]['callback_wl_redirect'];		// find the real callback
 	
 	$wl_options = get_option('widget_logic');						// do we want the widget?
 	$wl_value=($wl_options[$id])?stripslashes($wl_options[$id]):"true";
