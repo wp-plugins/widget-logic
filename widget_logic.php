5a5ebc3d6516e8373a6aa0a3ccef2badc22255dc
alanft <alanft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-12 19:45:23 +0000

    wp_reset_query option and multi wp_head fix too
    git-svn-id: https://plugins.svn.wordpress.org/widget-logic/trunk@125256 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget_logic.php
+++ widget_logic.php
@@ -63,7 +63,7 @@ function widget_logic_expand_control()
 	// check the 'widget content' filter option
 	if ( isset($_POST['widget_logic-options-submit']) )
 	{	$wl_options['widget_logic-options-filter']=$_POST['widget_logic-options-filter'];
-		$wl_options['widget_logic-options-rewind']=$_POST['widget_logic-options-rewind'];
+		$wl_options['widget_logic-options-wp_reset_query']=$_POST['widget_logic-options-wp_reset_query'];
 	}
 	update_option('widget_logic', $wl_options);
 }
@@ -79,11 +79,11 @@ function widget_logic_options_filter()
 			<h2>Widget Logic options</h2>
 			<p style="line-height: 30px;">
 
-			<label for="widget_logic-options-filter">Use 'widget_content' filter
+			<label for="widget_logic-options-filter" title="Adds a new WP filter you can use in your own code. Not needed for main Widget Logic functionality.">Use 'widget_content' filter
 			<input id="widget_logic-options-filter" name="widget_logic-options-filter" type="checkbox" value="checked" class="checkbox" <?php echo $wl_options['widget_logic-options-filter'] ?> /></label>
 				&nbsp;&nbsp;
-			<label for="widget_logic-options-rewind">Use 'rewind_posts' fix
-			<input id="widget_logic-options-rewind" name="widget_logic-options-rewind" type="checkbox" value="checked" class="checkbox" <?php echo $wl_options['widget_logic-options-rewind'] ?> /></label>
+			<label for="widget_logic-options-wp_reset_query" title="Resets a theme's custom queries before your Widget Logic is checked.">Use 'wp_reset_query' fix
+			<input id="widget_logic-options-wp_reset_query" name="widget_logic-options-wp_reset_query" type="checkbox" value="checked" class="checkbox" <?php echo $wl_options['widget_logic-options-wp_reset_query'] ?> /></label>
 
 			<span class="submit"><input type="submit" name="widget_logic-options-submit" id="widget_logic-options-submit" value="Save" /></span></p>
 		</form>
@@ -137,16 +137,19 @@ function widget_logic_redirect_callback()
 
 // the redirection comes here
 function widget_logic_redirected_callback()
-{	global $wp_registered_widgets;
+{	global $wp_registered_widgets, $wp_reset_query_is_done;
+
 	$params=func_get_args();											// get all the passed params
-
 	$id=array_pop($params);												// take off the widget ID
-
 	$callback=$wp_registered_widgets[$id]['callback_wl_redirect'];		// find the real callback
 	
 	$wl_options = get_option('widget_logic');							// do we want the widget?
 	$wl_value=($wl_options[$id])?stripslashes($wl_options[$id]):"true";
 	$wl_value=(stristr($wl_value, "return"))?$wl_value:"return (".$wl_value.");";
+
+	// before we execute the condtional code, perhaps we want to wp_reset_query...
+	if ($wl_options['widget_logic-options-wp_reset_query']=='checked' && !$wp_reset_query_is_done)
+	{	wp_reset_query(); $wp_reset_query_is_done=true;	}
 
 	$wl_value=(eval($wl_value) && is_callable($callback));
 	if ( $wl_value )
