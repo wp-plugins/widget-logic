cbf8e7bf5a00f0959add7f85d6ac57b60f85b0be
alanft <alanft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-12 16:45:44 +0000

    First attempt at adding 2.8-ish AJAX updating
    git-svn-id: https://plugins.svn.wordpress.org/widget-logic/trunk@125194 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget_logic.php
+++ widget_logic.php
@@ -4,11 +4,23 @@ Plugin Name: Widget Logic
 Plugin URI: http://freakytrigger.co.uk/wordpress-setup/
 Description: Control widgets with WP's conditional tags is_home etc
 Author: Alan Trewartha
-Version: 0.45
+Version: 0.46
 Author URI: http://freakytrigger.co.uk/author/alan/
 */ 
 
 
+add_filter( 'widget_update_callback', 'widget_logic_widget_update_callback', 10, 3); 
+
+// new in 2.8 - ajaxy update of a single widget
+function widget_logic_widget_update_callback($instance, $new_instance, $this_widget)
+{	$widget_id=$this_widget->id;
+	if ( isset($_POST[$widget_id.'-widget_logic']))
+	{	if((!$wl_options = get_option('widget_logic')) || !is_array($wl_options) ) $wl_options = array();
+		$wl_options[$widget_id]=$_POST[$widget_id.'-widget_logic'];
+		update_option('widget_logic', $wl_options);
+	}
+	return $instance;
+}
 
 add_action( 'sidebar_admin_setup', 'widget_logic_expand_control'); 
 
@@ -29,31 +41,30 @@ function widget_logic_expand_control()
 			if (!in_array($key, $regd_plus_new))
 				unset($wl_options[$key]);
 	}
-
-	else
-	// re-wire the registered control functions to go via widget_logic_extra_control
-	{	foreach ( $wp_registered_widgets as $id => $widget )
-		{	if (!$wp_registered_widget_controls[$id])
-					wp_register_widget_control($id,$widget['name'], 'widget_logic_empty_control');
-					
-			if (!array_key_exists(0,$wp_registered_widget_controls[$id]['params'])  || is_array($wp_registered_widget_controls[$id]['params'][0]))
-				$wp_registered_widget_controls[$id]['params'][0]['id_for_wl']=$id;
-			else
-			{	// some older widgets put number in to params directly (which messes up the 'templates' in WP2.5)
-				array_push($wp_registered_widget_controls[$id]['params'],$id);	
-				$wp_registered_widget_controls[$id]['height']+=40;					// this is really a pre2.5 thing - discard?
-			}
-
-			// do the redirection
-			$wp_registered_widget_controls[$id]['callback_wl_redirect']=$wp_registered_widget_controls[$id]['callback'];
-			$wp_registered_widget_controls[$id]['callback']='widget_logic_extra_control';		
+	
+	foreach ( $wp_registered_widgets as $id => $widget )
+	{	if (!$wp_registered_widget_controls[$id])
+				wp_register_widget_control($id,$widget['name'], 'widget_logic_empty_control');
+				
+		if (!array_key_exists(0,$wp_registered_widget_controls[$id]['params'])  || is_array($wp_registered_widget_controls[$id]['params'][0]))
+			$wp_registered_widget_controls[$id]['params'][0]['id_for_wl']=$id;
+		else
+		{	// some older widgets put number in to params directly (which messes up the 'templates' in WP2.5)
+			array_push($wp_registered_widget_controls[$id]['params'],$id);	
+			$wp_registered_widget_controls[$id]['height']+=40;					// this is really a pre2.5 thing - discard?
 		}
+
+		// do the redirection
+		$wp_registered_widget_controls[$id]['callback_wl_redirect']=$wp_registered_widget_controls[$id]['callback'];
+		$wp_registered_widget_controls[$id]['callback']='widget_logic_extra_control';		
 	}
+	
 	
 	// check the 'widget content' filter option
 	if ( isset($_POST['widget_logic-options-submit']) )
-		$wl_options['widget_logic-options-filter']=$_POST['widget_logic-options-filter'];
-
+	{	$wl_options['widget_logic-options-filter']=$_POST['widget_logic-options-filter'];
+		$wl_options['widget_logic-options-rewind']=$_POST['widget_logic-options-rewind'];
+	}
 	update_option('widget_logic', $wl_options);
 }
 
@@ -67,8 +78,13 @@ function widget_logic_options_filter()
 		<form method="POST">
 			<h2>Widget Logic options</h2>
 			<p style="line-height: 30px;">
-			<label for="widget_logic-options-filter">Use 'widget_content' filter?
+
+			<label for="widget_logic-options-filter">Use 'widget_content' filter
 			<input id="widget_logic-options-filter" name="widget_logic-options-filter" type="checkbox" value="checked" class="checkbox" <?php echo $wl_options['widget_logic-options-filter'] ?> /></label>
+				&nbsp;&nbsp;
+			<label for="widget_logic-options-rewind">Use 'rewind_posts' fix
+			<input id="widget_logic-options-rewind" name="widget_logic-options-rewind" type="checkbox" value="checked" class="checkbox" <?php echo $wl_options['widget_logic-options-rewind'] ?> /></label>
+
 			<span class="submit"><input type="submit" name="widget_logic-options-submit" id="widget_logic-options-submit" value="Save" /></span></p>
 		</form>
 	</div>
@@ -111,9 +127,11 @@ add_action('wp_head', 'widget_logic_redirect_callback');
 function widget_logic_redirect_callback()
 {	global $wp_registered_widgets;
 	foreach ( $wp_registered_widgets as $id => $widget )
-	{	array_push($wp_registered_widgets[$id]['params'],$id);
-		$wp_registered_widgets[$id]['callback_wl_redirect']=$wp_registered_widgets[$id]['callback'];
-		$wp_registered_widgets[$id]['callback']='widget_logic_redirected_callback';
+	{	if (!$wp_registered_widgets[$id]['callback_wl_redirect'])
+		{	array_push($wp_registered_widgets[$id]['params'],$id);
+			$wp_registered_widgets[$id]['callback_wl_redirect']=$wp_registered_widgets[$id]['callback'];
+			$wp_registered_widgets[$id]['callback']='widget_logic_redirected_callback';
+		}
 	}
 }
 
@@ -121,7 +139,9 @@ function widget_logic_redirect_callback()
 function widget_logic_redirected_callback()
 {	global $wp_registered_widgets;
 	$params=func_get_args();											// get all the passed params
+
 	$id=array_pop($params);												// take off the widget ID
+
 	$callback=$wp_registered_widgets[$id]['callback_wl_redirect'];		// find the real callback
 	
 	$wl_options = get_option('widget_logic');							// do we want the widget?
@@ -141,5 +161,4 @@ function widget_logic_redirected_callback()
 		}
 	}
 }
-
 ?>
